62f0d57ab05b1e7ac5eede95c4dd6a565270d734
Log2narottam409 <routnarottam409@gmail.com>
2021-01-06 18:00:06 +0530

    user.java

--- user.java
+++ user.java
@@ -1,15 +1,6 @@
 <<<<<<< HEAD
 Updated file
-Git command are vary important
-<<<<<<< HEAD
-This file contain user info
 
-git branch contain branch name
-=======
-Ihis file contain user info
-Git command are vary important
-git branch comtain branch name
->>>>>>> parent of 5e38b11 (user.java added)
 =======
 Updated code
 testing
